dde41fb06e3472cc925f35fdea5b2cad3622e7de
user <user@ubuntu.localdomain>
2012-05-29 16:33:22 +1000

    Wrote new java program to take messages to send

---
+++ mote/roboSender.java
@@ -0,0 +1,96 @@
+import net.tinyos.message.*;
+import net.tinyos.util.*;
+import java.io.*;
+import java.util.Arrays;
+
+//Type "make"
+//Then run serialforwarder: java net.tinyos.sf.SerialForwarder -comm serial@/dev/ttyUSB1:iris
+
+
+public class roboSender implements MessageListener
+{
+
+	MoteIF mote;
+
+	void run() {
+		//System.out.println("Starting motes");
+		mote = new MoteIF(PrintStreamMessenger.err);
+		//mote.registerListener(new RobotMessaging(),this);
+		//System.out.println("Motes started");
+
+		//Forever, take input from stdin.
+		while(true)
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    	String command = null;
+			try {
+				command = br.readLine();
+			} catch (IOException e) {
+				System.out.println("Error!");
+				System.exit(1);
+			}
+
+			command = command.trim();
+			String[] splitCommand = command.split(" ");
+			if (splitCommand.length >= 8)
+			{
+				Short mode = Short.valueOf(splitCommand[0]);
+				Short cmd = Short.valueOf(splitCommand[1]);
+				Short cmd_duration = Short.valueOf(splitCommand[2]);
+				Short ack = Short.valueOf(splitCommand[3]);
+				Short req_info = Short.valueOf(splitCommand[4]);
+				Short current_x = Short.valueOf(splitCommand[5]);
+				Short current_y = Short.valueOf(splitCommand[6]);
+				Short hit_wall = Short.valueOf(splitCommand[7]);
+
+				sendMessage(mode,cmd,cmd_duration,ack,req_info,current_x,current_y,hit_wall);
+			} else {
+				System.out.println("Message not sent, not enough arguments provided.");
+			}
+		}
+
+
+
+	}
+
+
+    public synchronized void messageReceived(int dest_addr, 
+            Message msg) {
+
+    	//Do nothing.
+
+    }
+
+	public void sendMessage(Short mode, Short cmd, Short cmd_duration,
+		Short ack, Short req_info, Short current_x, Short current_y,
+		Short hit_wall)
+	{
+		RobotMessaging rm = new RobotMessaging();
+		rm.set_mode(mode);
+		rm.set_cmd(cmd);
+		rm.set_cmd_duration(cmd_duration);
+		rm.set_ack(ack);
+		rm.set_req_info(req_info);
+		rm.set_current_x(current_x);
+		rm.set_current_y(current_y);
+		rm.set_hit_wall(hit_wall);
+	    try {
+	        mote.send(MoteIF.TOS_BCAST_ADDR, rm);
+	    } catch (IOException e) {
+	        System.out.println("Error sending message");
+	        return;
+	    }
+	    System.out.println("Message sent");
+
+	}
+
+
+
+	public static void main(String[] args)
+	{
+		roboSender me = new roboSender();
+		me.run();
+		System.out.println("running");
+	}
+
+}
--- mote/roboTalker.java
+++ mote/roboTalker.java
@@ -2,8 +2,10 @@ import net.tinyos.message.*;
 import net.tinyos.util.*;
 import java.io.*;
 
+
 //Type "make"
 //Then run serialforwarder: java net.tinyos.sf.SerialForwarder -comm serial@/dev/ttyUSB1:iris
+////java -cp /home/user/local/src/tinyos-2.x/support/sdk/java/tinyos.jar:/home/user/local/src/finalQtProject/finalProject/Final/mote roboTalker
 
 
 public class roboTalker implements MessageListener
@@ -15,9 +17,7 @@ public class roboTalker implements MessageListener
 		System.out.println("Starting motes");
 		mote = new MoteIF(PrintStreamMessenger.err);
 		mote.registerListener(new RobotMessaging(),this);
-		System.out.println("Motes started");
 	}
-
 
     public synchronized void messageReceived(int dest_addr, 
             Message msg) {
@@ -28,7 +28,6 @@ public class roboTalker implements MessageListener
 	        /* Update interval and mote data */
 	        //periodUpdate(omsg.get_version(), omsg.get_interval());
 	        System.out.println(rm.get_mode() + " " + rm.get_cmd() + " " + rm.get_cmd_duration() + " " + rm.get_ack() + " " + rm.get_req_info() + " " + rm.get_current_x() + " " + rm.get_current_y() + " " + rm.get_hit_wall());
-
 	        //data.update(omsg.get_id(), omsg.get_count(), omsg.get_readings());
 
 	        /* Inform the GUI that new data showed up */
@@ -36,9 +35,23 @@ public class roboTalker implements MessageListener
 	    }
     }
 
-	void sendMessage()
+	public void sendMessage()
 	{
 		RobotMessaging rm = new RobotMessaging();
+		rm.set_mode((short)1);
+		rm.set_cmd((short)0);
+		rm.set_cmd_duration((short)1000);
+		rm.set_ack((short)0);
+		rm.set_req_info((short)0);
+		rm.set_current_x((short)11);
+		rm.set_current_y((short)12);
+		rm.set_hit_wall((short)0);
+	    try {
+	        mote.send(MoteIF.TOS_BCAST_ADDR, rm);
+	    } catch (IOException e) {
+	        System.out.println("Error sending data");
+	    }
+
 
 	}
 
